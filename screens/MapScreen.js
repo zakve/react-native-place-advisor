39ffb65b09a626229344185f241b643a7aa8492a
zakve <zaklas.martin@gmail.com>
2019-10-24 19:08:49 +0200

    Add Screens

---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const MapScreen = props => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MapScreen;
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const PlaceDetailScreen = props => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default PlaceDetailScreen;
