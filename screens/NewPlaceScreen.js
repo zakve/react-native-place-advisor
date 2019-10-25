48eeb1a0b537dcba6a9b152e94e67715942ea2ad
zakve <zaklas.martin@gmail.com>
2019-10-25 12:13:04 +0200

    Add state to NewPlaceScreen

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, ScrollView } from "react-native";
 import { Input, Button, Text } from 'react-native-elements';
 
 const NewPlaceScreen = props => {
+    const [titleValue, setTitleValue] = useState('');
+
+    const titleChangeHandler = text => {
+        // add validation
+        setTitleValue(text);
+    }
+
+    const savePlaceHandler = () => {
+
+    }
+
     return (
         <ScrollView>
             <View style={styles.form}>
                 <Input
                     label='Add new place'
+                    onChangeText={titleChangeHandler}
+                    value={titleValue}
                 />
                 <Button
                     title="Save place"
                     style={styles.btn}
-                    onPress={() => { }}
+                    onPress={savePlaceHandler}
                 />
             </View>
         </ScrollView>
