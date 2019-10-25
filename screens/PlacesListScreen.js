b69c87b06291aa9d524f96b75b68fa7ed75ba499
zakve <zaklas.martin@gmail.com>
2019-10-25 15:05:43 +0200

    Add PlaceItem

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
+
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = props => {
+    const places = useSelector(state => state.places.places)
     return (
-        <View>
-            <Text>PlacesList screen</Text>
-        </View>
+        <FlatList data={places} keyExtractor={item => item.id} renderItem={itemData => <PlaceItem image={null} title={itemData.item.title} adress={null} onSelect={() => {
+            props.navigation.navigate('PlaceDetail', { placeTitle: itemData.item.title, placeId: itemData.item.id })
+        }} />} />
     )
 }
 
