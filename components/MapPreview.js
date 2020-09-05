5e22251e300fd3993c3f354dbb33b766d257dee2
zakve <zaklas.martin@gmail.com>
2020-09-05 10:21:20 +0200

    Update MapPreview

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Image, StyleSheet, View, ImageBackground } from 'react-native';
-import { GOOGLE_API_KEY } from '@env'
+import { StyleSheet, View } from 'react-native';
+import { Image } from 'react-native-elements';
+import Config from "react-native-config";
 
 const MapPreview = (props) => {
     let imagePreviewUrl;
 
     if (props.location) {
-        imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng},NY&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${GOOGLE_API_KEY}`
-        console.log(imagePreviewUrl)
+        imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng},NY&zoom=14&size=400x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${Config.GOOGLE_API_KEY}`
     }
 
     return <View>
         {
-            props.location ? <ImageBackground style={styles.mapImage} source={{ uri: imagePreviewUrl }} /> : props.children
+            props.location ? <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} /> : props.children
         }
     </View>
 }
